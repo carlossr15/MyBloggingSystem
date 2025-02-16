af08ca0ba45f7911c9e14d0bdffbd9310f44f847
carlossr15 <csobrado@ucm.es>
2025-02-16 19:01:47 +0100

    Firsts steps

---
+++ src/app/components/blogging-form/blogging-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blogging-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './blogging-form.component.html',
+  styleUrl: './blogging-form.component.css'
+})
+export class BloggingFormComponent {
+
+}
---
+++ src/app/components/blogging-list/blogging-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blogging-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './blogging-list.component.html',
+  styleUrl: './blogging-list.component.css'
+})
+export class BloggingListComponent {
+
+}
